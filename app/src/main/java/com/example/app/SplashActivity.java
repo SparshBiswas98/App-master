f96b8d44fe21b1a6abda5c2959cd7c4cefdb40ed
Sparsh Biswas <biswassparsh1@gmail.com>
2020-02-16 15:23:41 +0530

    Homepage+splash screen
    A splash screen has een added in the repo.
    A homepage has been created and is under development.

---
+++ app/src/main/java/com/example/app/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static int SPLASH_TIME_OUT=4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeintent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(homeintent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
+
